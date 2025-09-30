7d8236de986c7140de1ee5c6cc34f3d34d42f366
vijay <124682102+the-vijay-1001@users.noreply.github.com>
2025-09-30 15:22:27 +0530

    Create checkPallindrom.java
    check pallindrom

---
+++ checkPallindrom.java
@@ -0,0 +1,29 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.Scanner;
+
+class Test{
+    void isPallindrom(int num){
+        int reverse = 0;
+        while(num > 0){
+            reverse = reverse *10 + (num%10);
+            num = num/10;
+        }
+        if(reverse == num){
+            System.out.println("Yes It Is Pallindrom Value!!!!");
+        }
+        else{
+            System.out.println("This Is Not Pallindrom Value!!!");
+        }
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Length of the Loop : ");
+        int num = sc.nextInt();
+        Test obj = new Test();
+        obj.isPallindrom(num);
+    }
+}
