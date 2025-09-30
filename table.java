78cdfb0c2abc6fae2c1355b977cf2a2a7a27631b
vijay <124682102+the-vijay-1001@users.noreply.github.com>
2025-09-30 14:41:25 +0530

    Create table.java
    Create table.java

---
+++ table.java
@@ -0,0 +1,22 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.Scanner;
+
+class Test{
+    void printTable(int num){
+        for(int i = 1; i<= 10; i++){
+            System.out.println(num +" X "+ i + " = " + (num*i));
+        }
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Length of the Loop : ");
+        int num = sc.nextInt();
+        // System.out.println("You Have Enter a, b :" + a + " , "+ b);
+        Test obj = new Test();
+        obj.printTable(num);
+    }
+}
