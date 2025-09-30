f5862119b6db4bd90c836d4d0385161f63ca9180
vijay <124682102+the-vijay-1001@users.noreply.github.com>
2025-09-30 15:54:51 +0530

    Update checkPallindrom.java
    Update code of the pallindrom check

--- checkPallindrom.java
+++ checkPallindrom.java
@@ -4,12 +4,13 @@ import java.util.Scanner;
 
 class Test{
     void isPallindrom(int num){
+        int oldNum = num;
         int reverse = 0;
         while(num > 0){
             reverse = reverse *10 + (num%10);
             num = num/10;
         }
-        if(reverse == num){
+        if(reverse == oldNum){
             System.out.println("Yes It Is Pallindrom Value!!!!");
         }
         else{
